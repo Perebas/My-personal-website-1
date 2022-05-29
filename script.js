aa85cec5a5be4cf5551556bd76b51d9249244ba8
Wendel Santos <97599161+wendel-santos@users.noreply.github.com>
2022-05-28 22:35:47 -0300

    Update script.js
    The functionality to add and close the sidebar was added, the previous one from the old navbar was removed.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+const openNavBtn = document.querySelector('.open-nav-btn')
+const closeNavBtn = document.querySelector('.close-nav-btn')
+const nav = document.querySelectorAll('.nav')
+const overlayLeft = document.querySelector('.overlay-left')
+const scrollTopButton = document.querySelector('.scroll-top')
 
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav-visible');
+openNavBtn.addEventListener('click', () => {
+  nav.forEach(navElement => 
+    navElement.classList.add('visible')
+  )
+
+  overlayLeft.style.display = 'block'
+})
+
+closeNavBtn.addEventListener('click', () => {
+  nav.forEach(navElement => 
+    navElement.classList.remove('visible')
+  )
+
+  overlayLeft.style.display = 'none'
+})
+
+overlayLeft.addEventListener('click', () => {
+  nav.forEach(navElement => 
+    navElement.classList.remove('visible')
+  )
+
+  overlayLeft.style.display = 'none'
 })
